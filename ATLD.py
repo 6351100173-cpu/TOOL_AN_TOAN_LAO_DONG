61c3f5b7d8d1136d10653921d8ad696ad6f97793
6351100173-cpu <6351100173@st.utc2.edu.vn>
2026-09-16 23:50:10 +0700

    Update ATLD.py

--- ATLD.py
+++ ATLD.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
+from cong_nhan import cong_nhan_bp
 
 app = Flask(__name__)
+
+# Đăng ký chức năng Công nhân
+app.register_blueprint(cong_nhan_bp)
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
 if __name__ == "__main__":
     app.run(debug=False, use_reloader=False)
-
